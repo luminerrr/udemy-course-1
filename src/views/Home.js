0f24dc1120cf534c2f359fc137df13f022126b0b
Kevin <84540535+luminerrr@users.noreply.github.com>
2023-03-01 00:35:03 +0700

    adding hooks to fetch api

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,36 +1,11 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
 import Loader from '../components/Loader'
 import ProductCard from "../components/ProductCard";
+import useAxiosGet from "../hooks/HttpRequest";
 
 export default function Home() {
     let content = null;
     const url = `https://63fceb41859df29986c77da2.mockapi.io/products?page=1&limit=5`
-    const [products, setProducts] = useState({
-        loading:false,
-        data:null,
-        error:false
-    })
-
-    useEffect(()=>{
-        setProducts({
-            loading:true,
-            data:null,
-            error:false
-        })
-        axios.get(url)
-        .then(response => setProducts({
-            loading: false,
-            data: response.data
-        }))
-        .catch(()=>{
-            setProducts({
-                loading:false,
-                data:null,
-                error:true
-            })
-        })
-    }, [url])
+    let products = useAxiosGet(url)
 
     if(products.loading){
         content = <Loader />
--- src/views/Product.js
+++ src/views/Product.js
@@ -1,41 +1,15 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import Loader from "../components/Loader"
 import ProductCard from "../components/ProductCard"
+import useAxiosGet from "../hooks/HttpRequest"
 
 
 export default function Product(){
     const { id } = useParams()
     const url = `https://63fceb41859df29986c77da2.mockapi.io/products/${id}`
-    const [product, setProduct] = useState({
-        loading:false,
-        data:null,
-        error:false
-    })
-    
+    let product = useAxiosGet(url)
     let content = null
     
-    useEffect(()=>{
-        setProduct({
-            loading:true,
-            data:null,
-            error:false
-        })
-        axios.get(url)
-        .then(response => setProduct({
-            loading: false,
-            data: response.data
-        }))
-        .catch(()=>{
-            setProduct({
-                loading:false,
-                data:null,
-                error:true
-            })
-        })
-    }, [url])
-
     if(product.loading){
         content = <Loader />
     }
@@ -44,7 +18,6 @@ export default function Product(){
         content = <h1>Error Occurred</h1>
     }
     
-
     if(product.data){
         content =
             <ProductCard product = {product.data}/>
